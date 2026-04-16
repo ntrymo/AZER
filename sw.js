9980665e0c8e63a8a8862fc4f770a3e6aa20e007
ntrymo <mohakalieei1@gmail.com>
2026-04-16 20:29:51 +0100

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "accounts-v3";
+
+const urlsToCache = [
+  "index.html",
+  "petit.jpg",
+  "ski.jpg",
+  "mas.jpg"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
